b2a5d1041ec135e1caf33247420fcaf132188f8c
Vivek Amatya <vamat001@ucr.edu>
2020-10-29 11:07:12 -0700

    pg query handling api

---
+++ backend/queryHandler.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "database-1.c2hozwn3f7mo.us-west-2.rds.amazonaws.com",
+  database: "",
+  password: "job_hound123",
+  port: 5432,
+});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
+};
