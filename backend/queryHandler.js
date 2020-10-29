61526ffadf59dc75b6afbae6c8b1a08ac8812ddb
Vivek Amatya <vamat001@ucr.edu>
2020-10-29 14:21:43 -0700

    created db

--- backend/queryHandler.js
+++ backend/queryHandler.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "database-1.c2hozwn3f7mo.us-west-2.rds.amazonaws.com",
-  database: "",
+  database: "job_hound",
   password: "job_hound123",
   port: 5432,
 });
