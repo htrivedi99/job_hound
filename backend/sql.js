23c57f803acc6fd72bdc0224a0bd778bbb7f3aea
Carlos Garcia <49324580+cagarci@users.noreply.github.com>
2020-11-18 17:39:24 -0800

    Added 5 sql queries for job company table

--- backend/sql.js
+++ backend/sql.js
@@ -51,8 +51,107 @@ const removeJobPost = (request, response) => {
   );
 };
 
+// Company Queries
+
+//get all companies
+const getAllCompanies = (request, response) => {
+  db.query("SELECT FROM * company",(error, results) => {
+    if (error) 
+      response.status(400).json({ message: error });
+    else
+      response.status(200).json(results.rows);
+  });
+  db.end;  
+};
+
+//add a new a company to the table
+const addNewCompany = (request, response) => {
+  db.query(
+    "INSERT INTO company(name, size, rating, company_image) VALUES ($1, $2, $3, $4)",
+    [
+      request["name"],
+      request["size"],
+      request["rating"],
+      request["company_image"],
+      
+    ],
+    (error, result) => {
+      if (error) {
+        response.status(500).json({ message: error });
+      } else {
+        console.log(result);
+        response.status(201).json({ message: "Job added to DB" });
+      }
+    }
+  );
+  db.end;
+};
+
+//get companies where name starts with a given character
+const getCompanyByName = (request, response) => {
+  db.query("SELECT * FROM company WHERE name LIKE %$1",
+  [
+    request["name"],
+  ],
+  (error, result) => {
+    if (error){
+      response.status(500).json({ message: error});
+    }else{
+      console.log(result);
+      response.status(201).json({message: results.rows});
+    }
+  }
+);
+db.end
+};
+
+const 
+
+//get the company info given a company's rating where rating is between two given numeric values
+const getCompanyRatingRange = (request, response) => {
+  db.query(
+    "SELECT * FROM company WHERE $1 >= rating <= $2",
+    [
+      request["rating"],
+    ],
+    (error, result) => {
+      if (error){
+        response.status(500).json({ message: error});
+      }else{
+        console.log(result);
+        response.status(201).json({message: results.rows});
+      }
+    }
+  );
+  db.end
+};
+
+//update the comapny rating to a certain value given the name of the company
+const updateCompanyRatings = (request, response) => {
+  db.query("UPDATE company SET rating = $1 WHERE name = $2",
+  [
+    request["rating"],
+    request["name"]
+  ],
+  (error, result) => {
+    if (error){
+      response.status(500).json({ message: error});
+    }else{
+      console.log(result);
+      response.status(201).json({message: results.rows});
+    }
+  }
+  );
+  db.end;
+};
+
 module.exports = {
   getJobPosts,
   addNewJobPost,
   removeJobPost,
+  getAllCompanies,
+  addNewCompany,
+  getCompanyByName,
+  getCompanyRatingRange,
+  updateCompanyRatings,
 };
